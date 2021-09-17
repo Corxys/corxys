0bf8812f78330c6a74e2cd8fedeb6a83619cf3e0
Corxis <jennblngr@gmail.com>
2021-09-17 14:00:05 +0200

    style: minor adjustments to the style, and modification of the data set

--- src/data/projects.js
+++ src/data/projects.js
@@ -21,7 +21,7 @@ export const projects = [
       },
     ],
     image: {
-      src: 'https://lh3.googleusercontent.com/KHsYe2Oi6kN5tPibOaLhnj2ZtMJnZgMe9pn7nggxu8Riz4U0fkUtHMG9oggl6BRQB8fTEJOBW9AIt0leeo2MV7TXw4qONtUthpOyEYaem_yvdAfp51_gdzkyRUzrME-l9U3cp29Ce0Ve0VoCvIHMzJLNmwJbMnzBqL5uLojQZ2_mEde9DPqeSGdSurVo3n28mtOpfqhmXy4GQLJqvaJT6x9HB7S8WqHA-MwD0_0pJmMWLU6LAOvbD81VvfNe5w3PEFsehOkNJfnKxMl2XCdAB-5SFUKrQN2y9g9QWhTWN1uEEnlE4AWJpGj7cxrcScwniYChM1vj5e3GIzm-YCEGsEqnlB801QI72HRNR3uQt4O_WHocM02jxAZhOOcc8tyiJTeS8IV9Zi-gVZ4GZ7BBlVQ3zmRQtHasGIWaWf9NtxfSGOpeMPY_ZWAlPsVK2OBCO4H08uWjng0-zE2TlHLJ51fEXn-G965QlvedZgzPkIcZXoCimGgyqmt3J-7UhMaUpBAeuwqLrdyBYYKUlgW02ZmvPjS_ROjN-W_Eq-JCNVMbef4UBFYrc9M__cdFH5syj3ufyOsy3TF-cWML1NPiTenhwVnzq8zvk4DhsYbVF_WPwYJbAyRGpUMu1S-om-InqzvbV33w5cQXAlebfjCl-LRJhTgy90RrycZ_BoQRMWa8Y0bX_-KEgzrDRYV9e2G3ewwnZ56LHwe5AAW3ZV_UAHDVqA=w2016-h1512-no?authuser=0',
+      src: 'https://images.unsplash.com/photo-1628439900707-48d959859efe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80',
       alt: '',
     }
   },
@@ -45,7 +45,7 @@ export const projects = [
       },
     ],
     image: {
-      src: 'https://lh3.googleusercontent.com/Hp4U_7gNL244uqUdXyR8uNyQT2WEOBvco5DhWPoMH5duANAXT5we6EImoSgbXA78z9cfBxE4AgXHqb0RJs-EVz-KX79gpuMz57XpmuUevTBtimlcGI1oGIs6mIQqICm-2Z82BnnPxa8qH7B1vP9Ky508yHfwvccOreM031Wx43zGfqCtURK_4jL2UFh6WfAH49iUMVwIR1eN-j89U90IOjFj9K8Z8yrONWYPN5RgROf0olTZtSvSgORTL1CeQFNJNuok8dqJp1_8RrR1_ajsfW8gH-pzq3t_Pr6KvXoqBxawG7u2VIKjCT6TvKQjKXhGkkOMhIQ-GbdxkKZ8Y_Jgvgv1_BOskcumUPzHI-qC3qtj3eBIPk3qQ7yFU1t_qdR-5C5CNpmTVKGpNgE516JuTy7GdsTXJ-SkIP_8y_qKyed5NO9raQZtP_0DOa0Uc09HgEhO-3GF5eLTTZGaGZ-aEn50unujFbSmfcgI4FLF-ZcOwPAZAdua9RnOINPKmJoeZkfzOVANGRzZrkivAhtqs_ct_-ELjkt9EnS1e9dhWHqZ-eFXUsY43c_34G4JZNvE1nTLvHcdbJD6UQvyBmqtXEXAJGczVPhT3AkWyg92oWDyOZo8M3gxRZ28QX1q_xlEKgUko0d7qZrHTN9P7FT1MwenDQmVMresLUu36Ao1vBZFnI8VP5vyjoJSi9TbLDGyAmb_7crKoIrl-qqdslO3qL3o5Q=w333-h250-no?authuser=0',
+      src: 'https://images.unsplash.com/photo-1628439900707-48d959859efe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80',
       alt: '',
     } 
   },
@@ -69,7 +69,7 @@ export const projects = [
       },
     ],
     image: {
-      src: 'https://lh3.googleusercontent.com/c9vepOUpTKcpFPI9T3_2zWFtngCDx2EIJzExap51_es6FlAf5wMrqTQzwH2Mf5h40OPVA7I5pzFgwTPdH-w-q-iXA0bZgR0n_XuaxmvxRyPwJP0KrAnFz7CtDsKwB1TINkvo914I7YJ-1DYYgY52uCmCAGvBsojB63NbRKVMtfi9iHfv7r0oH-1QIrzcC74aGwvzUWd96ECKYnThXotO-l0pPJ0IpJhoaY_R8V539gs4IoKoXlrvjGw1OjZJ4gFLXPPvtrK4AAR2c1KARu7C1Ult_ikJAKx88dwyWGDUOuNadVf7lmP41eYWseT08_jEVgEd0rzpN6qzDXPQPUwD7ErLFmVkdt2p4UABaK3LII81R-f2z_a0EAcF7C_r4ISsrzKh4R-e-lcWk9kjvc3RF5PyaCALobEYMPt-0c4bE3bRuvM6YSGKnlkLqEgKy-UUzTCOaCUAWmsCb0nla7_LTYmQMY8DUU9H8MTB69s3tuV2zC9Ljiv2h18vNv5bgoIxpGITuTfyp0W9FAgcYOcog5DZxJ0zcS1NY9unTxxa9tFCPoSFdXRIiHc3DOVFOww3GnDQSSJP8HPRML0DjYUWfH967ajHJHbBDd5IRcH24Uh4ODrAJd_31tS_0ggAzIHn29y7WfiyApygXJ3uJifTWqeuLfeZ6Rm6gLXy8kTU9R1pkPRxGd7_LJP5cgpZbzWgACe9O-AzGblXp4vxsUvbNJa4PA=w2016-h1512-no?authuser=0',
+      src: 'https://images.unsplash.com/photo-1628439900707-48d959859efe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80',
       alt: '',
     } 
   },
@@ -93,7 +93,7 @@ export const projects = [
       },
     ],
     image: {
-      src: 'https://lh3.googleusercontent.com/BOo8vZqNooDJVopMY3PUHyNQp7NMyuknQevL00Kv846s_DgdDhYehrCIjXxxCw66oV-9lIH7LRYHIljd9vYHEHqzuEHTjuTH4ttdtjGjU3Yf_TaTKGeauuGnNoelExgf64KeF12XITXtozmMQo3M3XqXJUL3HeLULmz0WII20p0OjgXj-8hyBXItYSQJIfvsDx97FWJbDpykGNy58nigaiZ5itligG3Zin3Zzw0cG4g1LBWYSxE-bESdHw5vqSkYfmYxxAbjf-3iHahPkjopEA4DKCUoatl0fPTfHfzcrBAXShj_5eZdktnGHjJ9fY2fWoXSmCS3n_2BIrXG1DTYD5VVL-tzUn90Ha-8IwwKEBNUZz3ePW0L__-agN51wpVXFkj2jWhBmmi4i9ok2kjSUOKBph1oygUIhn5HfoVop86RO9TEuNuStkou8iLx0OL_hSemT-qVK64mP18-14to-o6NuWkjCvG9t7LjaFDsNlGxK2vSXpuFB7elAGgaZP68DRamal3l15bCznGKuXe7ugIjN5n9v_dlG7-opBxvzRt7DavQY3zaHHaIBBrJZbmKmCOGf-hpnFZg5L34zidauOAu2joogaIQtkiNrY-Op6revzDzL-gpkDrAdsJdGSnYNs_m-o5EWbHaLl47IDlxe8dv02w-ZGAQi5kMFshUzZhU7hLaYSXp3kNlF2AbRXhISGYyB6Im4HkRMplr-OK_79UCuw=w2016-h1512-no?authuser=0',
+      src: 'https://images.unsplash.com/photo-1628439900707-48d959859efe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80',
       alt: '',
     } 
   },
